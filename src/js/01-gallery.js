623b8f4fe09647e4216fdc79fadd40629ca32283
makoviisergii <sergiimakovii78@gmail.com>
2023-01-31 15:11:36 +0200

    31.01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,38 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import SimpleLightbox from '//node_modules/simplelightbox/dist/simple-lightbox.esm';
+
+const list = document.querySelector('ul.gallery');
+const galleryArr = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>`
+  )
+  .join('');
+
+list.insertAdjacentHTML('beforeend', galleryArr);
+
+function openOrigImag(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
