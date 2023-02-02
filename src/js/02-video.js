eb5232612cccb25f2ef52cd5e67d7025d71cb74f
makoviisergii <sergiimakovii78@gmail.com>
2023-02-02 19:31:57 +0200

    hw_8_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
-player.on('timeupdate');
+import Player from '@vimeo/player';
+
+import throttle from 'lodash.throttle';
+
+const frame = document.querySelector('#vimeo-player');
+
+const player = new Player(frame);
+
+let seconds = localStorage.getItem('videoplayer-current-time');
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+if (seconds) {
+  player.setCurrentTime(seconds);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
 
+// const LOCALSTORAGE_KEY = 'goit-example-message';
+
+// updateOutput();
+// form.addEventListener('submit', saveMessage);
+
+// function saveMessage(evt) {
+//   evt.preventDefault();
+//   localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
+//   updateOutput();
+//   form.reset();
+// }
+
+// function updateOutput() {
+//   output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || '';
+// }
